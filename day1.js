d09e41184c2d76106d7b090474b7775a2d592cf9
Canttwo <anjayharis@gmail.com>
2020-05-18 15:46:52 +0700

    menambahkan komentar

--- day1.js
+++ day1.js
@@ -1,1 +1,15 @@
-//test push1
+//Pseudocode//
+
+//Langka-langkah merebus air//
+
+//saya disini akan menjelaskan langkah-langkah cara merebus air, semoga bermanfaat
+
+
+//langkah pertama: masukan air kedalam teko sampai penuh
+//langkah kedua: letakan teko berisi air tersebut keatas kompor
+// langkah ketiga: nyalakan kompor
+//langkah ke-empat: tunggu beberapa saat sampai mendidih
+//langkah ke-lima: setelah air mendidih matikan kompor dan angkat teko berisi air panas
+
+
+//sekian tutorial dari saya, terima kasih ^^//
